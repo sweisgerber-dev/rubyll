f2322b285528e0802d893f57b0e85f494585949b
Sebastian Weisgerber <weisgerber@cispa.saarland>
2018-11-27 09:56:21 +0100

    Fixed template path to be more meaningful for rubyll

--- run.template.sh
+++ run.template.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 HOSTNAME="rubyll"
-HOST_PATH_1="/PATH/TO/aosp-7.1"
-DOCKER_PATH_1="/aosp/aosp-7.1"
+HOST_PATH_1="/PATH/TO/project"
+DOCKER_PATH_1="/workspace/project"
 
 docker run -ti \
     --hostname ${HOSTNAME} \
